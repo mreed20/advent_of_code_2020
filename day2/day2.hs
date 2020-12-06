b86eb5fc3fa3f038082e017e36a87174d7a0bcae
Michael Reed <michael@michaelreed.io>
2020-12-06 13:49:41 -0500

    day 2 part 2

--- day2/day2.hs
+++ day2/day2.hs
@@ -2,7 +2,6 @@
 
 import qualified Data.Text.IO as T
 import qualified Data.Text as T
-import qualified Text.Read as T
 
 -- A password is just a list of characters
 type Password = T.Text
@@ -21,12 +20,26 @@ data PasswordEntry = PasswordEntry PasswordPolicy Password
 main :: IO ()
 main = do
   entries <- readPasswordEntries "input.txt"
-  print . length . filter isEntryValid $ entries
+  let l1 = length . filter validPasswordPart1 $ entries
+  putStrLn $ "part 1: " ++ show l1
+  let l2 = length . filter validPasswordPart2 $ entries
+  putStrLn $ "part 2: " ++ show l2
 
-isEntryValid :: PasswordEntry -> Bool
-isEntryValid (PasswordEntry (PasswordPolicy min max c) password) =
+validPasswordPart1 :: PasswordEntry -> Bool
+validPasswordPart1 (PasswordEntry (PasswordPolicy min max c) password) =
   let n = T.count (T.singleton c) password
   in n >= min && n <= max
+
+xor :: Bool -> Bool -> Bool
+xor a b = if a then not b else b
+
+validPasswordPart2 :: PasswordEntry -> Bool
+validPasswordPart2 (PasswordEntry (PasswordPolicy i1 i2 c) password) =
+  let containsFirst  = T.index password (i1-1) == c
+      containsSecond = T.index password (i2-1) == c
+  -- exactly one of the positions must contain the password
+  in xor containsFirst containsSecond
+
 
 parseRange :: T.Text -> (Int, Int)
 parseRange s = let [low, high] = T.splitOn "-" s
