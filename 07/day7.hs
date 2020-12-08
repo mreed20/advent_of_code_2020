d66ebca120d81e7d6c281742a8c3109c66eaa8e9
Michael Reed <michael@michaelreed.io>
2020-12-08 15:44:09 -0500

    sort

--- 07/day7.hs
+++ 07/day7.hs
@@ -1,25 +1,25 @@
-
 {-# LANGUAGE OverloadedStrings #-}
 
 module Day7 where
 
 -- TODO: use Text.Parsec.Token
 
-import Text.Parsec.String ( Parser )
-import Text.Parsec
-    ( digit,
-      lower,
-      spaces,
-      string,
-      many1,
-      sepBy,
-      (<|>),
-      parse,
-      try )
 import Data.Char (digitToInt)
-import Data.Functor ( ($>) )
-import Data.Graph ( graphFromEdges, path, vertices )
+import Data.Functor (($>))
+import Data.Graph (graphFromEdges, path, vertices)
 import Data.Maybe (fromJust)
+import Text.Parsec
+  ( digit,
+    lower,
+    many1,
+    parse,
+    sepBy,
+    spaces,
+    string,
+    try,
+    (<|>),
+  )
+import Text.Parsec.String (Parser)
 
 -- A Bag has a color and can contain 1 or more other types of Bags.
 
@@ -37,31 +37,35 @@ main = do
     Right bags ->
       putStrLn $ "part 1: " ++ show (part1 bags)
 
-
 -- represent the bags as a graph
 part1 :: [Bag] -> Int
-part1 bag = let edges = map (\(color, childBags) -> (color, color, childBags)) bag
-                (graph, _, vertexFromKey) = graphFromEdges edges
-                goldVertex = fromJust $ vertexFromKey "shiny gold"
-                -- get all the vertices in the graph that aren't shiny gold
-                vs = filter (/= goldVertex) $ vertices graph
-                canReachGold v = path graph v goldVertex
-            in length . map canReachGold $ vs
+part1 bags =
+  let bagToEdge (color, childBags) = (color, color, childBags)
+      (graph, _, vertexFromKey) = graphFromEdges (map bagToEdge bags)
+      goldVertex = fromJust $ vertexFromKey "shiny gold"
+      -- get all the vertices in the graph that aren't shiny gold
+      vs = filter (/= goldVertex) $ vertices graph
+      canReachGold v = path graph v goldVertex
+   in length . map canReachGold $ vs
 
 bag :: Parser Bag
 bag = do
-    c <- color
-    token "bags"
-    token "contain"
-    childColors <- (try (string "no other bags") $> []) <|> childBags
-    token "."
-    return (c, childColors)
+  c <- color
+  token "bags"
+  token "contain"
+  childColors <- (try (string "no other bags") $> []) <|> childBags
+  token "."
+  return (c, childColors)
 
 childBags :: Parser [String]
-childBags = (do number
-                c <- color
-                try (token "bags") <|> try (token "bag")
-                return c) `sepBy` token ","
+childBags =
+  ( do
+      number
+      c <- color
+      try (token "bags") <|> try (token "bag")
+      return c
+  )
+    `sepBy` token ","
 
 color :: Parser String
 color = do
@@ -84,7 +88,7 @@ token s = lexeme (string s)
 -- Run the given parser, ignoring leading and trailing spaces
 lexeme :: Parser a -> Parser a
 lexeme p = do
-    spaces
-    v <- p
-    spaces
-    return v
+  spaces
+  v <- p
+  spaces
+  return v
