4d8386ae51090370d58dc0fed21a4bc9e2959303
Michael Reed <michael@michaelreed.io>
2020-12-10 18:18:30 -0500

    day 7: use megaparsec
    much more pleasant to use than Parsec

--- 07/day7.hs
+++ 07/day7.hs
@@ -2,36 +2,38 @@
 
 module Day7 where
 
-import Data.Char (digitToInt)
 import Data.Functor (($>))
 import Data.Graph (graphFromEdges, path)
 import Data.List (delete)
 import Data.Maybe (fromJust)
-import Text.Parsec
-  ( digit,
-    lower,
-    many1,
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Data.Void (Void)
+import Text.Megaparsec
+  ( Parsec,
     parse,
     sepBy,
-    spaces,
-    string,
-    try,
+    some,
     (<|>),
   )
-import Text.Parsec.String (Parser)
+import qualified Text.Megaparsec.Char as C
+import qualified Text.Megaparsec.Char.Lexer as L (decimal, lexeme, symbol)
+import Text.Megaparsec.Error (errorBundlePretty)
+
+type Parser = Parsec Void T.Text
 
 -- A Bag has a color and a list of bag names (and their quantity) it can contain.
-type Bag = (String, [(String, Int)])
+type Bag = (T.Text, [(T.Text, Int)])
 
 main :: IO ()
 main = do
-  input <- readFile "input.txt"
+  input <- T.readFile "input.txt"
   -- Finally, I found a good use for mapM! this results
   -- in bags having the type `Either ParseError [Bag]`,
   -- whereas with `map` the type is `[Either ParseError Bag]`.
-  case mapM (parse bag "") (lines input) of
+  case parse bags "" input of
     Left x ->
-      print x
+      putStrLn . errorBundlePretty $ x
     Right bags -> do
       putStrLn $ "part 1: " ++ show (part1 bags)
       putStrLn $ "part 2: " ++ show (part2 bags)
@@ -55,56 +57,53 @@ part2 bags = do
   follow start bags
 
 -- Wow this took WAY too long to figure out.
-follow :: [(String, Int)] -> [Bag] -> Maybe Int
+follow :: [(T.Text, Int)] -> [Bag] -> Maybe Int
 follow [] _ = return 0
 follow cs bags = do
-  let recurse (name, c) = do {b <- lookup name bags;
-                              r <- follow b bags;
-                              return $ c + c * r}
+  let recurse (name, c) = do
+        b <- lookup name bags
+        r <- follow b bags
+        return $ c + c * r
   x <- mapM recurse cs
   return $ sum x
 
+bags :: Parser [Bag]
+bags = bag `sepBy` C.newline
+
 bag :: Parser Bag
 bag = do
-  c <- color
-  token "bags"
-  token "contain"
-  childColors <- (try (string "no other bags") $> []) <|> childBags
-  token "."
+  c <- lexeme color
+  string "bags"
+  string "contain"
+  childColors <- (string "no other bags" $> []) <|> childBags
+  string "."
   return (c, childColors)
 
-childBags :: Parser [(String, Int)]
-childBags =
-  ( do
-      n <- number
-      c <- color
-      try (token "bags") <|> try (token "bag")
-      return (c, n)
-  )
-    `sepBy` token ","
+childBags :: Parser [(T.Text, Int)]
+childBags = childBag `sepBy` string ","
 
-color :: Parser String
+childBag :: Parser (T.Text, Int)
+childBag = do
+  n <- integer
+  c <- color
+  string "bags" <|> string "bag"
+  return (c, n)
+
+color :: Parser T.Text
 color = do
-  c1 <- lexeme $ many1 lower
-  c2 <- lexeme $ many1 lower
-  return $ c1 ++ " " ++ c2
+  c1 <- lowerString
+  c2 <- lowerString
+  return . T.concat $ [c1, " ", c2]
 
--- Parse a number in the range [low, high].
-number :: Parser Int
-number = lexeme positiveNatural
-  where
-    -- https://stackoverflow.com/questions/10726085/how-do-i-get-parsec-to-let-me-call-read-int
-    positiveNatural :: Parser Int
-    positiveNatural = foldl (\a i -> a * 10 + digitToInt i) 0 <$> many1 digit
-
--- Parse a string lexeme.
-token :: String -> Parser String
-token s = lexeme (string s)
-
--- Run the given parser, ignoring leading and trailing spaces
+-- helpers
 lexeme :: Parser a -> Parser a
-lexeme p = do
-  spaces
-  v <- p
-  spaces
-  return v
+lexeme = L.lexeme C.hspace
+
+string :: T.Text -> Parser T.Text
+string = L.symbol C.hspace
+
+integer :: Parser Int
+integer = lexeme L.decimal
+
+lowerString :: Parser T.Text
+lowerString = T.pack <$> (lexeme . some $ C.lowerChar)
