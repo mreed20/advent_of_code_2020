140731b50d091f7b10df526cee60afd997b14372
Michael Reed <michael@michaelreed.io>
2020-12-17 17:42:20 -0500

    day 8: much simpler without state monad

--- src/Day08.hs
+++ src/Day08.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Day08 where
 
@@ -55,43 +55,29 @@ solve = do
        in show $ part1 arr
 
 part1 :: Array Int Instr -> Int
-part1 instrs =
-  let finalState = execState (eval instrs) mkEvalState
-   in getAcc finalState
+part1 instrs = getAcc $ eval instrs mkEvalState
 
 -- Evaluate the sequence of instructions, stopping before
 -- we evaluate an instruction twice.
-eval :: Array Int Instr -> State EvalState ()
-eval instrs = do
+-- eval :: Array Int Instr -> State EvalState ()
+eval :: Array Int Instr -> EvalState -> EvalState
+eval instrs s@(EvalState ii acc executed)
   -- Only keep going if the instruction has not been executed already.
-  --
-  -- I'm not 100% clear on wy we need FlexibleContexts here, but I
-  -- believe it's because the constraints on modifyII/Acc have concrete
-  -- type variables in them: e.g., they are of the form MonadError Failure m
-  -- instead of MonadError a m. See here for more info: https://stackoverflow.com/questions/22795768/non-type-variable-argument-in-the-constraint-monaderror-failure-m
-  let modifyII fi = modify $ \s -> s { getII = fi (getII s)} 
-  let modifyAcc fa = modify $ \s -> s { getAcc = fa (getAcc s)} 
-  let modifyExecuted f = modify $ \s -> s { getExecuted = f (getExecuted s)} 
-  executed <- gets getExecuted
-  ii <- gets getII
-  when (ii `notElem` executed) $ do
-    -- record this instruction
-    modifyExecuted (ii:)
-    -- Evaluate the next instruction.
-    case instrs ! ii of
-      -- Increment the instruction index by one.
-      Nop -> modifyII (+1)
-      -- Increment the instruction index by one
-      -- and the accumulator by the given amount
-      Acc inc -> modifyII (+1) *> modifyAcc (+inc)
-      -- Add off to instruction index.
-      Jmp off -> modifyII (+off)
-    -- make recursive call
-    eval instrs
-
--- eval instrs Nop       = (1, 0)
--- eval instrs (Acc inc) = (1, inc)
--- eval instrs (Jmp off) = (off, 0)
+  | ii `elem` executed = s
+  | otherwise =
+    let modifyII f s = s {getII = f (getII s)}
+        modifyAcc f s = s {getAcc = f (getAcc s)}
+        modifyExecuted f s = s {getExecuted = f (getExecuted s)}
+        -- determine the next state
+        s' = case instrs ! ii of
+          -- Increment the instruction index by one.
+          Nop -> modifyII (+ 1) s
+          -- Increment the instruction index by one
+          -- and the accumulator by the given amount
+          Acc inc -> modifyII (+ 1) . modifyAcc (+ inc) $ s
+          -- Add off to instruction index.
+          Jmp off -> modifyII (+ off) s
+     in eval instrs (modifyExecuted (ii :) s')
 
 {-
 -- parsers
